9a9f91b4b52c86d4c62c631303b993300c762f8c
ssun0611 <rlathfdl463@naver.com>
2017-04-11 20:19:18 +0900

    #1 Frame xml

---
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/frametest/MainActivity.java
@@ -0,0 +1,19 @@
+package rlathfdl463.kr.hs.emirim.frametest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button but1,but2,but3;
+        I
+
+    }
+}
